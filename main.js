90accbc0e6ecacfb342da97c1d52581bb6a85371
Charles Ogeta <charlesogetah@gmail.com>
2026-03-16 17:58:22 +0300

    index.html

---
+++ main.js
@@ -0,0 +1,17 @@
+function showDetails(){
+alert("Employee Details: This is a placeholder for employee information.");
+}
+
+function editContact(){
+alert("Edit functionality will be implemented here.");
+}
+
+function deleteContact(){
+
+let confirmDelete = confirm("Are you sure you want to delete?");
+
+if(confirmDelete){
+alert("Employee deleted (placeholder).");
+}
+
+}
