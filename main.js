1cdcef42fc0d7067531972bd59b07a7e2a51fa7b
Charles Ogeta <charlesogetah@gmail.com>
2026-03-25 16:32:46 +0300

    addition

--- main.js
+++ main.js
@@ -10,4 +10,7 @@ function deleteContact(){
 if(confirm("Are you sure you want to delete?")){
 alert("Contact deleted");
 }
+function moreInfo(){
+alert("Employee Management System developed to manage employee contacts.");
+}
 }
